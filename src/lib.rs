3ffc6deadf19413b287204afe8cd21f165839eb2
Stevie Alvarez <stevie.6strings@gmail.com>
2022-08-17 00:39:08 -0400

    add graph insert node test

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,21 @@ mod graph;
 
 #[cfg(test)]
 mod graph_tests {
-  use crate::graph::Graph;
+  use crate::graph::{Graph, Node};
 
   #[test]
   fn new_graph() {
     let result: Graph<i32, i32> = Graph::new();
     assert_eq!(result.is_empty(), true);
+  }
+
+  #[test]
+  fn insert_node() {
+    let mut g: Graph<i32, i32> = Graph::new();
+    let n1: Node<i32, i32> = Node::new(0, 0);
+    let n2: Node<i32, i32> = Node::new(0, 0);
+    assert!(g.insert(n1));
+    assert!(!g.insert(n2));
   }
 }
 

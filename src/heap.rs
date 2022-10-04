ee84d73383100b5bb27f07d0eac895523cbce76f
Stevie Alvarez <stevie.6strings@gmail.com>
2022-10-04 13:50:18 -0400

    insert stub

--- src/heap.rs
+++ src/heap.rs
@@ -6,5 +6,14 @@ pub struct Heap<T> {
 }
 
 impl<T> Heap<T> where T: Eq + PartialEq+ PartialOrd + Debug + Display {
+  /// Create a new empty heap.
+  pub fn new() -> Heap<T> {
+    let h: Heap<T> = Heap { nodes: Vec::new(), };
+    h
+  }
 
+  /// Insert a unique element into the heap.
+  pub fn insert(e: T) -> bool {
+    false  // TODO: Implement
+  }
 }
